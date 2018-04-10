114789d88aa81a1cad154ae452bf287eba9a752e
annyhiss <animashaunoluwatosin7@gmail.com>
2018-04-10 22:38:16 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>my figma design</title>
+	<link rel="stylesheet" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+</head>
+<body>
+	<section id="home">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 col-sm-12">
+					<h1 class="text">This Is My Figma <br> Design</h1>
+					<a href="#" class=" btn btn-default btn-center">Discover More</a>
+				</div>
+			</div>
+		</div>
+		<div class="d_t">   
+			<p> Date/Time: MM/DD/YYYY hh:mm
+				<?php 
+				echo date('l jS \of F Y h:i:s A');
+				?>
+			</p>
+		</div>
+
+
+
+
+
+
+	</section>
+</body>
+</html>
+
+
+
